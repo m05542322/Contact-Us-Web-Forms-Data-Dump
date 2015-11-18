61e7cc0a36aee5ce90fcad8b0f31b7b8e841111b
th98 <tim.h.huang@newegg.com>
2015-11-18 14:07:50 +0800

    add attachment attribure to email class

--- main.php
+++ main.php
@@ -7,33 +7,36 @@ require_once 'functions.php';
 $fileList = array("All", "Tech_Support", "Other");
 
 $arrayToExcel = array();
+$result = array();
 foreach ($fileList as $file){
-  $handle = fopen($file.'.txt', "r");
-  if ($handle) {
-      while (($line = fgets($handle)) !== false) {
-          // data[0] = No, data[1] = Form Type, data[2] = User Data, data[3] = Modify Time, data[4] = Created Time,
-          $data = preg_split('/\t+/', $line);
-          $data[2] = preg_replace('/\\\\\\\"/', ' ', $data[2]);
-          $data[2] = preg_replace('/","/', "\n", $data[2]);
-          $data[2] = preg_replace('/[{}"]/', '', $data[2]);
-          $data[2] = preg_replace('/\\\\\\\\\//', '/', $data[2]);
-          //echo $data[2];
-          //die();
-          $result = array(
-             "Form Type" => $data[1],
-             "Content" => $data[2]
-          );
-          $arrayToExcel[] = $result;
-      }
-      fclose($handle);
-  } else {
-      // error opening the file.
-  }
-  //var_dump($arrayToExcel);
-  exportArrayToXlsx($arrayToExcel, array(
-      "filename" => $file.'.xls',
-      "title" => "Sheet 1"
-  ));
+    $result = null;
+    $arrayToExcel = null;
+    $handle = fopen($file.'.txt', "r");
+    if ($handle) {
+        while (($line = fgets($handle)) !== false) {
+            // data[0] = No, data[1] = Form Type, data[2] = User Data, data[3] = Modify Time, data[4] = Created Time,
+            $data = preg_split('/\t+/', $line);
+            $data[2] = preg_replace('/\\\\\\\"/', ' ', $data[2]);
+            $data[2] = preg_replace('/","/', "\n", $data[2]);
+            $data[2] = preg_replace('/[{}"]/', '', $data[2]);
+            $data[2] = preg_replace('/\\\\\\\\\//', '/', $data[2]);
+            //echo $data[2];
+            //die();
+            $result = array(
+                "Form Type" => $data[1],
+                "Content" => $data[2]
+            );
+            $arrayToExcel[] = $result;
+        }
+        fclose($handle);
+    } else {
+        // error opening the file.
+    }
+    //var_dump($arrayToExcel);
+    exportArrayToXlsx($arrayToExcel, array(
+        "filename" => $file.'.xls',
+        "title" => "Sheet 1"
+    ));
 }
 
 sendMailWithDownloadUrl('Contact-Us-Web-Form-Data', $fileList);
