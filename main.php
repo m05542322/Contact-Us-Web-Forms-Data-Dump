54971692bc0bedaf869ad65fea6c1631055e3adc
th98 <tim.h.huang@newegg.com>
2015-11-18 15:00:35 +0800

    add attachment attribure to email class

--- main.php
+++ main.php
@@ -4,6 +4,8 @@ require_once 'PHPExcel/Classes/PHPExcel.php';
 require_once 'phpQuery/phpQuery/phpQuery.php';
 require_once 'functions.php';
 
+$debug = false;
+
 $now = date('Y-m-d');
 $fileList = array("All", "Tech_Support", "Other");
 $fileListWithDate = array();
